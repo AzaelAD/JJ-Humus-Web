9b302daf92bd811dfa8a160410c52b17b88febc6
JhovanyGabrielJaimeFernandez <jhovanygabriel@hotmail.com>
2025-02-12 21:00:17 -0700

    Primera revisión

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { PopularProductsComponent } from './components/popular-products/popular-products.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    CategoriesComponent,
+    PopularProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([]),
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/popular-products/popular-products.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-popular-products',
+  standalone: false,
+  templateUrl: './popular-products.component.html',
+  styleUrl: './popular-products.component.css',
+})
+export class PopularProductsComponent {
+  products = [
+    { name: "Peppermint Carbo - Vit", description: "Beneficios Es un excelente precursor de fitobentos en los fondos del estanque, los cuales son...", image: "https://www.carbo-vit.com/imagenes/carbovit.png" },
+    { name: "Vit-Amin", description: "El Humofosfato de calcio en conjunto con el ión potasio realizan la producción de fitoalexina radi...", image: "https://www.carbo-vit.com/imagenes/Fvit-amin.png" },
+    { name: "Xurgén", description: "Nutriente Bio Orgánico concentrado con alto contenido en aminoácidos y péptidos que en conjunto c...", image: "https://www.carbo-vit.com/imagenes/Fxurgen.png" }
+  ];
+}
